56eef88dc2d2f80841ed8c83ef2f89375782d2bd
ahassanein <allaa-hassanein@uiowa.edu>
2014-04-05 21:58:28 -0500

    fixed bug on view change

--- src/com/example/android/iClimb/ConfigurationActivity.java
+++ src/com/example/android/iClimb/ConfigurationActivity.java
@@ -154,18 +154,15 @@ public class ConfigurationActivity extends Activity {
             Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
             finish();
             return;
-        }
+        }        
         
         //setnodes();
        
     }
     
 	/**
-	 * 
-	 * /**
      * This Method will call all saved nodes and will add an ontouch listener to each node that will respond to each touch by saving
      * the current address to the touched node and sending a message to the hub requesting the next address be sent.
-     * 
      * */
     
     private void setnodes(){
@@ -281,7 +278,6 @@ public class ConfigurationActivity extends Activity {
 				Intent switchView = new Intent(this, ClimbActivity.class);
 				startActivity(switchView);
 		        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
-		        
 			break;
 			case R.id.action_start_config:
 				setnodes();
@@ -339,17 +335,6 @@ public class ConfigurationActivity extends Activity {
         mOutStringBuffer = new StringBuffer("");
     }
 
-    @Override
-    public synchronized void onPause() {
-        super.onPause();
-        if(D) Log.e(TAG, "- ON PAUSE -");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if(D) Log.e(TAG, "-- ON STOP --");
-    }
 
     @Override
     public void onDestroy() {
